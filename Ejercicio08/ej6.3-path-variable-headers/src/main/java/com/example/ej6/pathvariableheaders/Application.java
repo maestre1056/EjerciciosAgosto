5d6f91cd2cb60bf93d9a4de19d1b31bcc643aa6a
antonio1056 <antonio.maestre@bosonit.com>
2022-08-25 10:37:01 +0200

    variables

---
+++ Ejercicio08/ej6.3-path-variable-headers/src/main/java/com/example/ej6/pathvariableheaders/Application.java
@@ -0,0 +1,13 @@
+package com.example.ej6.pathvariableheaders;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
